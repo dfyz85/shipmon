96f269e9e54fcd958b32e74578afeae197bb926c
Ivan <492587@gmail.com>
2019-12-26 13:25:51 +0300

    Update MERINETRAFIK-React

--- databaseconnection.py
+++ databaseconnection.py
@@ -1,5 +1,6 @@
 import pymongo
 import logging
+from bson.son import SON
 
 client = pymongo.MongoClient("mongodb://dfyz:rtyfghvbn65@briese-shard-00-00-vryeg.mongodb.net:27017,briese-shard-00-01-vryeg.mongodb.net:27017,briese-shard-00-02-vryeg.mongodb.net:27017/test?ssl=true&replicaSet=briese-shard-0&authSource=admin&retryWrites=true&w=majority")
 brieseDb = client['shipsBriese']
@@ -7,8 +8,18 @@ shipsPossition = brieseDb['shipsPosition']
 vesselsName = brieseDb['shipsData']
 
 def dbVesselsName():
-
-  return vesselsName.find(no_cursor_timeout=True)
+  pipeline = [
+    {"$group":
+        {
+          "_id": "$imo",
+          "count":SON([("$sum",1)]),
+          "vesselName":{"$first":"$vesselName"}
+        } 
+    },
+    {"$sort":SON([("count", 1)])}
+  ]
+  db = shipsPossition.aggregate(pipeline)
+  return db
 
 def dbInsertVessel(data):
   try:
@@ -29,3 +40,7 @@ def dbEditShipsData():
 }
   vesselsName.update_many({},newValues)
 
+
+# db = dbVesselsName() 
+# for i in db:
+#   print(i['count'])
--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import logging
 import argparse
 import sys
 from os import getlogin
+#import brotli
 import re
 import time
 from datetime import datetime
@@ -12,7 +13,7 @@ from databaseconnection import dbInsertVessel, dbVesselsName
 
 def createParser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument ('-d', '--delay', default=100, type=int)
+    parser.add_argument ('-d', '--delay', default=10, type=int)
     return parser
 
 user = getlogin()
@@ -21,44 +22,113 @@ logging.info(f"Start by {user}.")
 parser = createParser()
 namespace = parser.parse_args(sys.argv[1:])
 vesselsName = dbVesselsName()
+shipsId = {
+    '9513622': '365335',
+    '9537264': '758448',
+    '9513634': '365673',
+    '9504724': '365852',
+    '9569530': '365385',
+    '9415325': '365280',
+    '9508421': '366133',
+    '9418999': '365089',
+    '9436965': '364391',
+    '9508407': '152497', 
+    '9571399': '366127',
+    '9435856': '5579407',
+    '9454228': '206287',
+    '9578763': '366406',
+    '9563744': '366760',
+    '9508469': '6036997',
+    '9508380': '365195',
+    '9256327': '205264',
+    '9508471': '366591',
+    '9508419': '152612',
+    '9362621': '362460',
+    '9508457': '366400',
+    '9501679': '366455',
+    '9504750': '1037050',
+    '9347061': '360406',
+    '9261994': '366085', 
+    '9435026': '363667',
+    '9504736': '365858',
+    '9301134': '364867', 
+    '9297096': '204277', '9435868': '5457938', '9357212': '363233', '9266308': '361271', '9563720': '366536', '9571375': '365654', '9195420': '205655', '9362633': '204335', '9468114': '755460', '9436331': '364731', '9501265': '364716', '9508483': '366701', '9303314': '363452', '9427079': '363211', '9375252': '364517', '9256315': '204901', '9433315': '152782', '9433327': '364165', '9421130': '125683', '9434589': '364815', '9421116': '363291', '9378230': '363557', '9501277': '364720', '9251509': '206340', '9569528': '364979', '9421166': '122987', '9508304': '364952', '9563732': '366543', '9196931': '366099', '9421178': '125906', '9563706': '365848', '9454216': '364435', '9508433': '6037775', '9378242': '363881', '9436953': '363889', '9363508': '205543', '9571387': '366121', '9468102': '758239', '9428205': '365461', '9347059': '359926', '9504798': '366553', '9266310': '361336', '9504786': '366542', '9588598': '755952', '9566796': '206417', '9501681': '366459', '9421142': '125907', '9427081': '363397', '9505302': '3568908', '9386976': '205994', '9501667': '365892', '9504748': '5682360', '9434761': '364269', '9501203': '206156', '9815331': '5707562', '9566784': '365354', '9437153': '364893', '9815329': '5600177', '9700392': '3350145', '9436329': '206229', '9501655': '365884', '9360477': '299843', '9341108': '204134', '9578751': '366521', '9437165': '364895', '9505405': '204265', '9347047': '362806', '9552020': '365615', '9505326': '206420', '9505338': '206271', '9513658': '365684', '9289790': '204946', '9505314': '206386', '9197818': '204431', '9815317': '5396907', '9323651': '205252', '9195470': '205527', '9815343': '5784185', '9434759': '364086', '9281786': '204671', '9386988': '364127', '9195482': '205586', '9434577': '364220', '9505297': '4690815', '9197765': '203867', '9374387': '205623', '9551662': '365606', '9252931': '204537', '9505285': '204182', '9369526': '205665', '9319430': '205378', '9195377': '204416'}
+# xxx = {}
+# for  i in vesselsName:
+#     imo = i['_id']
+#     url = 'https://www.marinetraffic.com/en/photos/of/ships/imo:'+imo
+#     print(url)
+#     headers = {
+#             #'Host':	'www.marinetraffic.com',
+#             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
+#             'Accept': '*/*',
+#             #'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+#             #'Accept-Encoding': 'br',
+#             #'X-Requested-With': 'XMLHttpRequest',
+#             'DNT': '1',
+#             'Connection': 'keep-alive',
+#             #'Referer': 'Referer: https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,time_of_latest_position,lat_of_latest_position,lon_of_latest_position&imo|eq|imo='+str(imo) ,
+#             #'Cookie':'__cfduid=d9211ff4bab3ac41408e9cdc110acde951577308491; SERVERID=app5; _ga=GA1.2.1865478285.1577308493; _gid=GA1.2.1372905508.1577308493; _cmpQcif3pcsupported=1; _hjid=7ffc513c-b25d-4461-a959-66ef5a23a55b; hsfirstvisit=; __hstc=153128807.8eec19da8abb58bb97188c619547396b.1577308498600.1577308498600.1577308498600.1; hubspotutk=8eec19da8abb58bb97188c619547396b; __hssrc=1; __hssc=153128807.11.1577308498600; _fbp=fb.1.1577308499503.1875851644; __atuvc=3%7C52; __atuvs=5e03d8bc21bdef7e002; vTo=1; _gat=1',
+#             'TE': 'Trailers',
+#          }
+#     r = requests.get(url, headers = headers).text
+#     soup = BeautifulSoup(r,features="lxml")
+#     vesselName = soup.find('div',id='gallery').find_all('a')
+#     shipID = vesselName[0].get('href').split('shipid:')[1]
+#     xxx.update({imo:shipID}) 
+# print(xxx)      
 for  i in vesselsName:
     try:
-        imo = i['imo']
-        url = 'https://www.marinetraffic.com/en/ais/details/ships/imo:'+ imo
+        reordingTime = datetime.now()
+        imo = shipsId[str(i['_id'])]
+        urlPosition = 'https://www.marinetraffic.com/vesselDetails/latestPosition/shipid:'+imo
+        urlDetails = 'https://www.marinetraffic.com/vesselDetails/voyageInfo/shipid:'+imo
+        urlInfo = 'https://www.marinetraffic.com/en/vesselDetails/vesselInfo/shipid:'+imo
         #print(url)
         headers = {
-        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20103103 Firefox/64.0'
-        }
+            'Host':	'www.marinetraffic.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
+            'Accept': '*/*',
+            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+            #'Accept-Encoding': 'gzip, deflate,',
+            'X-Requested-With': 'XMLHttpRequest',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            #'Referer': 'https://www.marinetraffic.com/en/ais/details/ships/shipid:365335/vessel:BBC%20LIMA',
+            #'Cookie':'_ga=GA1.2.395253674.1437139472; __atuvc=0%7C42%2C26%7C43%2C0%7C44%2C0%7C45%2C10%7C46; __atssc=google%3B17; __hstc=153128807.1e7f97c74a950808d850ae602ebaea4f.1516606452153.1573590744826.1577301361670.25; hubspotutk=1e7f97c74a950808d850ae602ebaea4f; __zlcmid=qchyoos9UHeJXq; __cfduid=d354d627fc06eecbac8ad398ba5a5b16b1560189640; _hjid=59d845fa-caad-43bb-8e30-6ca2bd4c2c89; _fbp=fb.1.1571665909504.2045023531; __gads=ID=59ef9a5d4a2498e6:T=1571755513:S=ALNI_MZbFmVyAZ2zHTEZUXweYDLvsrL-BA; _gaexp=GAX1.2.gqwNE1cuQkmlx_ZHXmcLZg.18279.1; SERVERID=app4; vTo=1; hsfirstvisit=; __hssrc=1; dmxRegion=false; CAKEPHP=3f0o3uctpodu805hit2s0gf3ag; AUTH=EMAIL=pythonsev@gmail.com&CHALLENGE=RQ1pziOwRO489MQIky1e; mt_user[UserID]=2285832; _gid=GA1.2.1262382283.1577301354; _cmpQcif3pcsupported=1; __hssc=153128807.16.1577301361670; _gat=1',
+            'TE': 'Trailers',
+         }
         #connectTor()
         time.sleep(namespace.delay)
-        r = requests.get(url, headers = headers).text.encode('utf-8')
-        soup = BeautifulSoup(r,features="lxml")
-        vesselName = soup.find('h1',{'class':'font-200 no-margin'}).text
+        rPosition = requests.get(urlPosition, headers = headers).json()
+        rDetails = requests.get(urlDetails, headers = headers).json()
+        rInfo = requests.get(urlInfo, headers = headers).json()
+        vesselName = rInfo['name']
         #if vesselName == "BBC ROMANIA":
-        #    print(soup.find('div',{'160% line-110'}).attrs)
-        arrival = soup.find('div',{'class':'160% line-110 text-default text-light'})['title']
-        departure = soup.find('div',{'160% line-110'})['title']
-        timeStamp = soup.find('div',{'class':'table-cell cell-full collapse-768'}).find('div', {'class':'group-ib'}).find('strong').text.split('(')[0].replace('utc','')
-        if 'ago' in timeStamp:
-            timeStamp = soup.find('div',{'class':'table-cell cell-full collapse-768'}).find('div', {'class':'group-ib'}).find('strong').text.split('(')[1].replace('utc','')
+        #    print(soup.find('div',{'160% line-110'}).attrs) 
+        if rDetails['arrivalPort']:
+            arrival =  f"{rDetails['arrivalPort']['name']} [{rDetails['arrivalPort']['countryCode']}]"
+            if rDetails['arrivalPort']['timestamp']:
+                eta =  str(datetime.fromtimestamp(rDetails['arrivalPort']['timestamp']))
+        if rDetails['departurePort']:
+            departure =  f"{rDetails['departurePort']['name']} [{rDetails['departurePort']['countryCode']}]"
+            if rDetails['departurePort']['timestamp']:
+                atd =  str(datetime.fromtimestamp(rDetails['departurePort']['timestamp']))     
+        timeStamp = str(datetime.fromtimestamp(rPosition['lastPos']))
+        if 'ago' or 'now' in str(timeStamp).lower():
+            timeStamp = str(reordingTime)[:-10]
         # JS-code data = new Date("2019-07-14 18:30")
-        tags = soup.find('div',{'class':'table-cell cell-full collapse-768'}).find_all('div', {'class':'vertical-offset-10 group-ib'})
-        area = tags[1].text.split('Area:')[1].strip()
-        posittionLat = tags[2].find('a', {'class':'details_data_link'}).text.split()[0][:-1]
-        posittionLon = tags[2].find('a', {'class':'details_data_link'}).text.split()[2][:-1]
-        status = soup.find(text=re.compile("(Moored|Underway Using Engine|Undefined|At Anchor)"))
-        speedCourse = tags[4].find('strong').text.replace('°','').replace('kn','').replace(' ','').split('/')
-        speed = speedCourse[0]
-        course = speedCourse[1]
-        draughtTag = soup.find('table',{'class':'table table-striped table-striped-alt table-condensed voyage-related table-aftesnippet table-aftesnippet-primary'})
-        try:
-            draught = draughtTag.find_all('span', {'class':'text-default'})[0].find('b').text.replace('m','')
-        except AttributeError:
-            draught = None
+        area = rPosition['areaCode']
+        posittionLat = rPosition['lat']
+        posittionLon = rPosition['lon']
+        status = rPosition['shipStatus']
+        speed = rPosition['speed']
+        course = rPosition['course']
+        if rDetails['draughtReported']:
+            draught = round(rDetails['draughtReported'],2)
         #print(draught)
         #print(vesselName, '\n',timeStamp,'\n', status,'\n',posittionLat,' ',posittionLon,'\n',area)
-        myHash = hashlib.md5(vesselName.encode('utf-8')+posittionLat.encode('utf-8')+posittionLon.encode('utf-8')).hexdigest()
-        reordingTime = datetime.now()
+        myHash = hashlib.md5(vesselName.encode('utf-8')+str(posittionLat).encode('utf-8')+str(posittionLon).encode('utf-8')).hexdigest()
         data = {
             '_id':myHash,
             'vesselName':vesselName,
@@ -73,9 +143,12 @@ for  i in vesselsName:
             'course':course,
             'draught':draught,
             'departure':departure,
-            'arrival':arrival
+            'arrival':arrival,
+            'atd':atd,
+            'eta':eta
         }
-        dbInsertVessel(data)
+        print(data)
+        #dbInsertVessel(data)
     except Exception as e:
         logging.error(f"Open by {user}. Vessels name:{vesselName}", exc_info=True)
 logging.info(f"Stop by {user}.")
