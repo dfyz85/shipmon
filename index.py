6b469f2f6eb0363c44ee5c2611a481aee620dcb9
Ivan <492587@gmail.com>
2020-02-18 11:41:34 +0300

    Optimize 2.0

--- index.py
+++ index.py
@@ -10,17 +10,20 @@ from datetime import datetime
 import hashlib
 from bs4 import BeautifulSoup
 from databaseconnection import dbInsertVessel, dbVesselsName
-
+#TIME DELAY
 def createParser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument ('-d', '--delay', default=10, type=int)
+    parser.add_argument ('-d', '--delay', default=0, type=int)
     return parser
+#TIME DELAY
 
 user = getlogin()
 logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO, datefmt='%d-%b-%y %H:%M:%S')
 logging.info(f"Start by {user}.")
+#TIME DELAY
 parser = createParser()
 namespace = parser.parse_args(sys.argv[1:])
+#TIME DELAY
 vesselsName = dbVesselsName()
 shipsId = {
     '9513622': '365335',
@@ -56,6 +59,19 @@ shipsId = {
     '9570668':'366189',
     '9570670':'366191',
     }
+headers = {
+            'Host':	'www.marinetraffic.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
+            'Accept': '*/*',
+            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+            #'Accept-Encoding': 'gzip, deflate,',
+            'X-Requested-With': 'XMLHttpRequest',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            #'Referer': 'https://www.marinetraffic.com/en/ais/details/ships/shipid:365335/vessel:BBC%20LIMA',
+            #'Cookie':'_ga=GA1.2.395253674.1437139472; __atuvc=0%7C42%2C26%7C43%2C0%7C44%2C0%7C45%2C10%7C46; __atssc=google%3B17; __hstc=153128807.1e7f97c74a950808d850ae602ebaea4f.1516606452153.1573590744826.1577301361670.25; hubspotutk=1e7f97c74a950808d850ae602ebaea4f; __zlcmid=qchyoos9UHeJXq; __cfduid=d354d627fc06eecbac8ad398ba5a5b16b1560189640; _hjid=59d845fa-caad-43bb-8e30-6ca2bd4c2c89; _fbp=fb.1.1571665909504.2045023531; __gads=ID=59ef9a5d4a2498e6:T=1571755513:S=ALNI_MZbFmVyAZ2zHTEZUXweYDLvsrL-BA; _gaexp=GAX1.2.gqwNE1cuQkmlx_ZHXmcLZg.18279.1; SERVERID=app4; vTo=1; hsfirstvisit=; __hssrc=1; dmxRegion=false; CAKEPHP=3f0o3uctpodu805hit2s0gf3ag; AUTH=EMAIL=pythonsev@gmail.com&CHALLENGE=RQ1pziOwRO489MQIky1e; mt_user[UserID]=2285832; _gid=GA1.2.1262382283.1577301354; _cmpQcif3pcsupported=1; __hssc=153128807.16.1577301361670; _gat=1',
+            'TE': 'Trailers',
+         }
 # xxx = {}
 # for  i in vesselsName:
 #     imo = i['_id']
@@ -83,32 +99,21 @@ shipsId = {
 for  i in vesselsName:
     try:
         reordingTime = datetime.now()
-        imo = shipsId[str(i['_id'])]
+        imo = shipsId.pop(str(i['_id']))
         urlPosition = 'https://www.marinetraffic.com/vesselDetails/latestPosition/shipid:'+imo
-        urlDetails = 'https://www.marinetraffic.com/vesselDetails/voyageInfo/shipid:'+imo
-        urlInfo = 'https://www.marinetraffic.com/en/vesselDetails/vesselInfo/shipid:'+imo
-        #print(url)
-        headers = {
-            'Host':	'www.marinetraffic.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
-            'Accept': '*/*',
-            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-            #'Accept-Encoding': 'gzip, deflate,',
-            'X-Requested-With': 'XMLHttpRequest',
-            'DNT': '1',
-            'Connection': 'keep-alive',
-            #'Referer': 'https://www.marinetraffic.com/en/ais/details/ships/shipid:365335/vessel:BBC%20LIMA',
-            #'Cookie':'_ga=GA1.2.395253674.1437139472; __atuvc=0%7C42%2C26%7C43%2C0%7C44%2C0%7C45%2C10%7C46; __atssc=google%3B17; __hstc=153128807.1e7f97c74a950808d850ae602ebaea4f.1516606452153.1573590744826.1577301361670.25; hubspotutk=1e7f97c74a950808d850ae602ebaea4f; __zlcmid=qchyoos9UHeJXq; __cfduid=d354d627fc06eecbac8ad398ba5a5b16b1560189640; _hjid=59d845fa-caad-43bb-8e30-6ca2bd4c2c89; _fbp=fb.1.1571665909504.2045023531; __gads=ID=59ef9a5d4a2498e6:T=1571755513:S=ALNI_MZbFmVyAZ2zHTEZUXweYDLvsrL-BA; _gaexp=GAX1.2.gqwNE1cuQkmlx_ZHXmcLZg.18279.1; SERVERID=app4; vTo=1; hsfirstvisit=; __hssrc=1; dmxRegion=false; CAKEPHP=3f0o3uctpodu805hit2s0gf3ag; AUTH=EMAIL=pythonsev@gmail.com&CHALLENGE=RQ1pziOwRO489MQIky1e; mt_user[UserID]=2285832; _gid=GA1.2.1262382283.1577301354; _cmpQcif3pcsupported=1; __hssc=153128807.16.1577301361670; _gat=1',
-            'TE': 'Trailers',
-         }
-        #connectTor()
-        time.sleep(namespace.delay)
         rPosition = requests.get(urlPosition, headers = headers).json()
+        area = rPosition['areaCode']
+        areaName = rPosition['areaName']
+        posittionLat = rPosition['lat']
+        posittionLon = rPosition['lon']
+        status = rPosition['shipStatus']
+        speed = rPosition['speed']
+        course = rPosition['course']
+        timeStamp = str(datetime.fromtimestamp(rPosition['lastPos']))
+        if 'now' in str(timeStamp).lower():
+            timeStamp = str(reordingTime)[:-10]
+        urlDetails = 'https://www.marinetraffic.com/vesselDetails/voyageInfo/shipid:'+imo
         rDetails = requests.get(urlDetails, headers = headers).json()
-        rInfo = requests.get(urlInfo, headers = headers).json()
-        vesselName = rInfo['name']
-        #if vesselName == "BBC ROMANIA":
-        #    print(soup.find('div',{'160% line-110'}).attrs) 
         if rDetails['arrivalPort']:
             arrival =  f"{rDetails['arrivalPort']['name']} [{rDetails['arrivalPort']['countryCode']}]"
             if rDetails['arrivalPort']['timestamp']:
@@ -117,19 +122,21 @@ for  i in vesselsName:
             departure =  f"{rDetails['departurePort']['name']} [{rDetails['departurePort']['countryCode']}]"
             if rDetails['departurePort']['timestamp']:
                 atd =  str(datetime.fromtimestamp(rDetails['departurePort']['timestamp']))
-        timeStamp = str(datetime.fromtimestamp(rPosition['lastPos']))
-        if 'now' in str(timeStamp).lower():
-            timeStamp = str(reordingTime)[:-10]
-        # JS-code data = new Date("2019-07-14 18:30")
-        area = rPosition['areaCode']
-        areaName = rPosition['areaName']
-        posittionLat = rPosition['lat']
-        posittionLon = rPosition['lon']
-        status = rPosition['shipStatus']
-        speed = rPosition['speed']
-        course = rPosition['course']
         if rDetails['draughtReported']:
             draught = round(rDetails['draughtReported'],2)
+
+        # urlInfo = 'https://www.marinetraffic.com/en/vesselDetails/vesselInfo/shipid:'+imo
+        # rInfo = requests.get(urlInfo, headers = headers).json()
+        # vesselName = rInfo['name']
+        vesselName = str(i['vesselName'])
+        #print(url)
+        #connectTor()
+        #TIME DELAY
+        time.sleep(namespace.delay)
+        #TIME DELAY
+        #if vesselName == "BBC ROMANIA":
+        #    print(soup.find('div',{'160% line-110'}).attrs) 
+        # JS-code data = new Date("2019-07-14 18:30")
         #print(draught)
         #print(vesselName, '\n',timeStamp,'\n', status,'\n',posittionLat,' ',posittionLon,'\n',area)
         myHash = hashlib.md5(vesselName.encode('utf-8')+str(posittionLat).encode('utf-8')+str(posittionLon).encode('utf-8')+str(timeStamp)[:10].encode('utf-8')).hexdigest()
